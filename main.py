5da63c6df518f6043f8e69e3168b234928fcf249
Paul Cubitt <Paul Cubitt>
2017-01-10 21:01:28 +0000

    Add styling

--- main.py
+++ main.py
@@ -13,13 +13,16 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 
 ##### helper functions 
 
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
     def render_str(self, template, **params):
-        t = jinja_env.get_template(template)
-        return t.render(params)
+        return render_str(template, **params)
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
@@ -36,7 +39,6 @@ class Post(db.Model):
     last_modified = db.DateTimeProperty(auto_now = True)
 
     def render(self):
-        self._render_text = self.content.replace('\n', '<br>')
         return render_str("post.html", p = self)
 
 class MainPage(Handler):
